84eb3bc7b0b89cc689072c1bea8334ebd3b53ba4
IvanFromOdesa <krylosov382@gmail.com>
2022-09-18 19:19:14 +0300

    First Commit

---
+++ javascript_1.js
@@ -0,0 +1,57 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button') 
+let userChoice
+let computerChoice
+let result
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice // What is innerHTML in JS?
+                                            // The innerHTML property returns: 
+                                            // The text content of the element,
+                                            // including all spacing and inner HTML tags
+    generateComputerChoice()
+    getResult()
+}))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1 // from 1 to 3
+    if(randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+    if(randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+    if(randomNumber === 3) {
+        computerChoice = 'paper'
+    }
+
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+    if(computerChoice === userChoice) {
+        result = 'its a draw'
+    }
+    if(computerChoice === 'rock' && userChoice === 'paper') {
+        result = 'you win'
+    }
+    if(computerChoice === 'rock' && userChoice === 'scissors') {
+        result = 'you lost'
+    }
+    if(computerChoice === 'paper' && userChoice === 'scissors') {
+        result = 'you win'
+    }
+    if(computerChoice === 'paper' && userChoice === 'rock') {
+        result = 'you lost'
+    }
+    if(computerChoice === 'scissors' && userChoice === 'rock') {
+        result = 'you win'
+    }
+    if(computerChoice === 'scissors' && userChoice === 'paper') {
+        result = 'you lost'
+    }
+    resultDisplay.innerHTML = result
+}
